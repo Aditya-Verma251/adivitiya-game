b76cbbcbb9bf4e7cdbe5a09fe282646d198bfc3f
Aditya Kumar Verma <adityaverma10328@gmail.com>
2025-02-02 13:00:08 +0530

    made lev0 some more

--- src/level_0.gd
+++ src/level_0.gd
@@ -6,3 +6,9 @@ func _ready() -> void:
 	player = preload("res://src/player.tscn")
 	$Player._on_main_mpu()
 	
+
+
+func _on_glitch_trigger_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		
+	pass # Replace with function body.
