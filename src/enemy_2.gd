cac11b3ebc75d388725f029aa76f2f0f35f2fa19
harsh <harshyadav0072007@gmail.com>
2025-02-02 16:19:41 +0530

    made enemy 2 basics

---
+++ src/enemy_2.gd
@@ -0,0 +1,46 @@
+extends CharacterBody2D
+
+@export var time :float
+var proj = preload("res://src/enemy2projectile.tscn")
+var toShoot = false
+var player
+
+func shoot() -> void:
+	var projectile = proj.instantiate()
+	var d : Vector2 = player.position - position
+	d.normalized()
+	projectile.apply_impulse(d * 1)
+	$ProjectileContainer.add_child(projectile)
+
+func _ready() -> void:
+	$TimePeriod.one_shot = false
+	$TimePeriod.wait_time = time
+
+	
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	pass
+	
+
+
+func _on_time_period_timeout() -> void:
+	if toShoot:
+		shoot()
+	
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		player = body
+		shoot()
+		toShoot = true
+
+	
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		toShoot = false 
+		
