ee751343a68edf3a8d54de95c68b4adf5d3881a7
harsh <harshyadav0072007@gmail.com>
2025-02-02 18:56:33 +0530

    enemy 1&2

--- src/enemy_2_projectile.gd
+++ src/enemy_2_projectile.gd
@@ -1,6 +1,10 @@
 extends Sprite2D
 
+signal damage(val : float)
+
 var vel = Vector2.ZERO
+@export var e2 = "meow"
+@export var damageValue : float =5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,3 +16,14 @@ func _process(delta: float) -> void:
 	
 func move(dir:Vector2):
 	vel = dir.normalized()
+
+func setException(v):
+	e2 = v
+
+func _on_enemy_2_projectile_body_entered(body: Node2D) -> void:
+	print(e2)
+	if body.name == "Player":
+		damage.emit(damageValue) # Replace with function body.
+
+	if body.name != e2:
+		queue_free()
