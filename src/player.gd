65d9adce4c84164c0bcd1f69e8274babef5b144c
Aditya Kumar Verma <adityaverma10328@gmail.com>
2025-02-02 19:18:33 +0530

    added dash

--- src/player.gd
+++ src/player.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 
 @export var health : float = 100.0
 @export var g = Vector2(0, 98)
+var og
 @export var ospeed = 300.0
 @export var dashSpeed = 2000.0
 @export var max_speed = 1000.0
 var speed = 300.0
+var justDashed = false
+var endDash = false
+#@export var dashTime : float
 @export var jt = 1.0
 @export var acc = 0.0
 @export var jump_velocity = -400.0
@@ -13,6 +17,7 @@ static var isInputFixed = true
 
 func _ready() -> void:
 	$CollisionShape2D/AnimatedSprite2D.play()
+	og = g
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -38,10 +43,23 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		if speed < max_speed:
 			speed += acc
+		elif endDash:
+			pass
 		else:
 			speed = max_speed
+
+		if Input.is_action_just_pressed("dash") and not justDashed:
+			print("dash")
+			velocity.y = 0
+			speed +=  dashSpeed
+			g = Vector2.ZERO
+			justDashed = true
+			endDash = true
+			$DashTimer.start()
+			$DashTime.start()
 		
 		velocity.x = direction * speed
+
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		speed = 0
@@ -63,3 +81,12 @@ func _on_damage(damage: float) -> void:
 
 func gameOver() -> void:
 	pass
+
+
+func _on_dash_timer_timeout() -> void:
+	justDashed = false # Replace with function body.
+
+
+func _on_dash_time_timeout() -> void:
+	g = og
+	endDash = false# Replace with function body.
