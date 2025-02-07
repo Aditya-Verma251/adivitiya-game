b2197389dc1c503ec2e00dbd52a2d33371bf6a81
harsh <harshyadav0072007@gmail.com>
2025-02-07 08:46:53 +0530

    ahhhhhhhh

--- src/other/l_0_cutscene.gd
+++ src/other/l_0_cutscene.gd
@@ -2,7 +2,7 @@ extends Node
 
 func levelZeroCutscene():
 	GlobalVariables.isPaused = true
-	var script = ["meow", "woof"]
+	var script = ["Oh no, oh no, oh no… why is it doing that?", "Okay, okay, deep breath. Let’s fix this… probably.", "Try completing the obstecle course to\n figure out what is going on. \n help "]
 	$TextBox.displayText(script)
 	PlayerGeneralSignalManager.glitchyPosition.emit()
 	await  $TextBox.textDisplayed
